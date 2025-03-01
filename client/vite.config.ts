d7069360fecfabdd346b1ef033732f572cfe522e
Nikhil Kulkarni <85415160+nikhil-k-2001@users.noreply.github.com>
2025-03-02 01:36:42 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,3 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
-    },
-  },
-  base: process.env.NODE_ENV === "production" ? "/" : "/",
-  server: { port: 3000 },
-});
-
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
 // import path from "path";
@@ -26,6 +10,18 @@ export default defineConfig({
 //       "@": path.resolve(__dirname, "./src/"),
 //     },
 //   },
-//   base: "./", // ✅ Use relative paths for assets
+//   base: process.env.NODE_ENV === "production" ? "/" : "/",
 //   server: { port: 3000 },
 // });
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+    },
+  },
+  base: "./", // ✅ Use relative paths
+  server: { port: 3000 },
+});
+
