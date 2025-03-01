7cafccc31d3cbde25d9dc10c9b4298407a3f3f5c
Nikhil Kulkarni <85415160+nikhil-k-2001@users.noreply.github.com>
2025-03-02 00:18:16 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,18 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src/"),
+//     },
+//   },
+//   base: process.env.NODE_ENV === "production" ? "/google-sheets" : "/",
+//   server: { port: 3000 },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -10,6 +25,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src/"),
     },
   },
-  base: process.env.NODE_ENV === "production" ? "/google-sheets" : "/",
+  base: "./", // ✅ Use relative paths for assets
   server: { port: 3000 },
 });
