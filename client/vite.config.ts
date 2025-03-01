a2395dbd783408e8ea8f64f7e5ac15200108c196
Nikhil Kulkarni <85415160+nikhil-k-2001@users.noreply.github.com>
2025-03-02 01:55:47 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,8 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src/"),
-//     },
-//   },
-//   base: process.env.NODE_ENV === "production" ? "/" : "/",
-//   server: { port: 3000 },
-// });
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -21,7 +10,18 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src/"),
     },
   },
-  base: "./", // ✅ Use relative paths
+  base: process.env.NODE_ENV === "production" ? "/" : "/",
   server: { port: 3000 },
 });
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src/"),
+//     },
+//   },
+//   base: "./", // ✅ Use relative paths
+//   server: { port: 3000 },
+// });
 
